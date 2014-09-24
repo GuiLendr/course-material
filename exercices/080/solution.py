321e3d5faad6618fbf2757520cab86ca02e92487
Antoine Mazieres <Guillaume Lendrevie>
2014-09-24 18:48:22 +0200

    better this time

--- exercices/080/solution.py
+++ exercices/080/solution.py
@@ -9,8 +9,8 @@ Created on Tue Sep 23 10:05:30 2014
 import string
 alph = string.ascii_lowercase
 lettre = list(alph)
-for i in alph :
-    for j in alph :
-        if(i != j) :
-            if(i < j) :
+for i in alph:
+    for j in alph:
+        if(i != j):
+            if(i < j):
                 print(i+j, "\n")
