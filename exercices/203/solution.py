7ee80a8aa2f7d84d6473f3053abd2ff6fb626d37
Antoine Mazieres <Guillaume Lendrevie>
2014-09-24 21:22:51 +0200

    solution 203

---
+++ exercices/203/solution.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 24 21:16:04 2014
+
+@author: Guillaume
+"""
+
+
+def is_multiple(a, b):
+    if b % a == 0:
+        return('True')
+    else:
+        return('False')
+
