9478a281c898ac6fa29b1a55980621cfe25bc569
Antoine Mazieres <Guillaume Lendrevie>
2014-09-24 21:43:27 +0200

    better this time

--- exercices/110/solution.py
+++ exercices/110/solution.py
@@ -6,7 +6,10 @@ Created on Tue Sep 23 16:03:07 2014
 """
 
 import sys
-if len(sys.argv) == 4:
+if len(sys.argv) == 1:
+    m = "usage: python3 ./solution.py a_number (an_operator +-*/%^) a_number"
+    print(m)
+elif len(sys.argv) == 4:
     a = int(sys.argv[1])
     b = sys.argv[2]
     c = int(sys.argv[3])
