d10f0092a87e7f78c2fd3afec9423575a26ebc0e
Antoine Mazieres <Guillaume Lendrevie>
2014-09-24 18:35:50 +0200

    better this time

--- exercices/019/solution.py
+++ exercices/019/solution.py
@@ -5,5 +5,6 @@ Created on Mon Sep 22 15:53:14 2014
 @author: Guillaume
 """
 import sys
-res = int(sys.argv[1]) + int(sys.argv[2])
+if len(sys.argv) == 3:
+    res = int(sys.argv[1]) + int(sys.argv[2])
 print(res)
