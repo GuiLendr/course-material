3a9ce26b76a93e885c31438d9dae72a1e3d04b0b
Antoine Mazieres <Guillaume Lendrevie>
2014-09-24 18:54:13 +0200

    better this time

--- exercices/060/solution.py
+++ exercices/060/solution.py
@@ -7,7 +7,6 @@ Created on Mon Sep 22 18:07:51 2014
 
 alph = "abcdefghijklmnopqrstuvxyz"
 lettre = list(alph)
-for i in range (0, 25):
-    for j in range (0, 25):
-       print(lettre[i], lettre[j], "\n")
-       
+for i in range(0, 25):
+    for j in range(0, 25):
+        print(lettre[i], lettre[j], "\n")
