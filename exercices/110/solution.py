31af054893854098a1a2da2d7e03c6fe127abfca
Antoine Mazieres <Guillaume Lendrevie>
2014-09-24 19:09:33 +0200

    solut 110

---
+++ exercices/110/solution.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 23 16:03:07 2014
+
+@author: Guillaume
+"""
+
+import sys
+if len(sys.argv) == 4:
+    a = int(sys.argv[1])
+    b = sys.argv[2]
+    c = int(sys.argv[3])
+    res = 0
+    if (c == 0):
+        print("input error")
+    elif (b == "+"):
+        res = a + c
+    elif (b == "-"):
+        res = a - c
+    elif (b == "*"):
+        res = a * c
+    elif (b == "%"):
+        res = a % c
+    elif (b == "^"):
+        res = pow(a, c)
+    else:
+        print("input error")
+    print(res)
