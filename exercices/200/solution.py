4861e9a9909a74c62c68cc073f61b5ec087151aa
Antoine Mazieres <Guillaume Lendrevie>
2014-09-25 12:28:49 +0200

    better this time

--- exercices/200/solution.py
+++ exercices/200/solution.py
@@ -8,11 +8,11 @@ Created on Tue Sep 23 20:10:30 2014
 
 def is_prime(num):
     if ((num == 0) | (num == 1)):
-        return ("False")
+        return (False)
     else:
-        racine = int(pow(num, 0.5))
+        racine = int(pow(num, 0.5)) + 1
         for div in range(2, racine):
             if num % div == 0:
-                return ("False")
+                return (False)
             div = div + 1
-        return("True")
+        return(True)
